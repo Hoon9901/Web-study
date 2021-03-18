835b399e98293450aaec5ab58ebc9ed2ed5aadc2
Hooooooon <shonn2323@gmail.com>
2021-03-18 18:03:29 +0900

    자바스크립트 강좌 시작, 다크모드 추가

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function trasnNight(){
+    document.querySelector('body').style.backgroundColor = "black";
+    document.querySelector('body').style.color = "white";   
+}
+
+function transDay(){
+    document.querySelector('body').style.backgroundColor = "white";
+    document.querySelector('body').style.color = "black";   
+
+}
